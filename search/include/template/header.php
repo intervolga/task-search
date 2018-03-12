1cbd4741cc3f05f1cc53df5578eb91e0e12c46f5
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2018-03-12 11:45:35 +0300

    Добавлен собственный CSS

--- search/include/template/header.php
+++ search/include/template/header.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="stylesheet" href="/search/css/bootstrap.min.css">
+	<link rel="stylesheet" href="/search/css/custom.css">
 
 	<title>Тестовое задание intervolga.ru</title>
 </head>
