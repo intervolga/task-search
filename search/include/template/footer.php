03d8045eadd8b467784e5350b9c95be56b56cf6f
Anatoly Yerofeyev <yerofeyev.aa@gmail.com>
2018-03-12 11:43:58 +0300

    Добавлен шаблон страницы

---
+++ search/include/template/footer.php
@@ -0,0 +1,8 @@
+	<footer class="row">
+		<div class="col-md-6 offset-md-3 text-center">
+			Записей в базе: <?=countDatabase()?>
+		</div>
+	</footer>
+	</div><!--.container-->
+</body>
+</html>
---
+++ search/include/template/header.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>Тестовое задание intervolga.ru</title>
+</head>
+<body>
+<div class="container">
+	<div class="row">
+		<div class="col-md-6 offset-md-3">
+			<h1 class="text-center">Поиск</h1>
+		</div>
+	</div>
